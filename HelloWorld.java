c71780c8356bb2ed2781039d67604e577b12c7e5
Shristi Singhal <ss7377@srmist.edu.in>
2026-03-17 08:35:13 +0530

    Display Hello with Multiple Command-Line Arguments

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,8 +1,12 @@
 public class HelloWorld {
+
     public static void main(String[] args) {
-        String name ="World";
+
         if (args.length > 0) {
-            name = args[0];
-        }System.out.println("Hello " + name + "!");
+            String names = String.join(", ", args);
+            System.out.println("Hello, " + names + "!");
+        } else {
+            System.out.println("Hello, World!");
+        }
     }
 }
